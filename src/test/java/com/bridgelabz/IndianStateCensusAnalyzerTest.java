95d321013de1e454c3199e6113a8314a8fc480f1
Your Name <saifi.mujeeb2@gmail.com>
2023-05-29 23:55:53 +0530

    Added: welcome message on master branch

---
+++ src/test/java/com/bridgelabz/IndianStateCensusAnalyzerTest.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+import org.junit.Before;
+
+public class IndianStateCensusAnalyzerTest {
+
+    @Before
+    public void programStart(){
+        System.out.println("Welcome To Indian States Census Analyser Problem");
+    }
+
+}
